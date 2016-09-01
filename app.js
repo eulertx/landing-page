4e7e75a7ba23efce335a2602567da32b214205d1
fengxiaoping <fxp007@gmail.com>
2016-09-01 11:06:08 +0800

    copyright

--- app.js
+++ app.js
@@ -23,45 +23,91 @@ require('./cloud');
 app.use(AV.express());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.get('/', function(req, res) {
-  res.render('index', { currentTime: new Date() });
+app.get('/', function (req, res) {
+    res.render('index', {currentTime: new Date()});
 });
+
+var unirest = require("unirest");
+var cheerio = require("cheerio");
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
+app.get('/nlm', function (req, response, next) {
+    // var req = unirest("GET", "https://ghr.nlm.nih.gov/gene/AAAS");
+    var req = unirest("GET", req.query.url);
 
-app.use(function(req, res, next) {
-  // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
-  if (!res.headersSent) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  }
+    req.end(function (res) {
+        if (res.error) throw new Error(res.error);
+        var $ = cheerio.load(res.body);
+        var result = {};
+        result.name = $('h1.genes').text() || $('h1.health-conditions').text();
+        result.full_name = $('h2.gene-full-name').text();
+        $('div.hidden-print').remove();
+        $('div.related-info').remove();
+        $('div.expand-collapse-area').children('section').map(function (e, el) {
+            // return $(this).find('div.row').text();
+            // console.log($(this).attr('id'));
+            var section_id = $(this).children().last().attr('id');
+            result[section_id] = {};
+            $(this).find('ul').each(function (i, el) {
+                result[section_id].list = $(this).find('li').map(function (i, el) {
+                    //     return $(this).text();
+                    // }).get()
+                    return $(this).text();
+                }).get()
+            })
+
+            result[section_id].text = $(this).children().last().text().trim();
+            return $(this).children().last().attr('id');
+            // return $(this);
+        });
+
+        // result.normalfunction = $('div#normalfunction').text().trim();
+        // result.conditions = $('div#conditions').text().trim();
+        // result.location = $('div#location').text().trim();
+        // result.synonyms = $('div#synonyms').find('li').map(function (i, el) {
+        //     return $(this).text();
+        // }).get();
+        // result.sourcesforpage = $('div#sourcesforpage').find('li').map(function (i, el) {
+        //     return $(this).html();
+        // }).get();
+
+        response.send(result);
+    });
+});
+
+app.use(function (req, res, next) {
+    // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
+    if (!res.headersSent) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    }
 });
 
 // error handlers
-app.use(function(err, req, res, next) { // jshint ignore:line
-  var statusCode = err.status || 500;
-  if(statusCode === 500) {
-    console.error(err.stack || err);
-  }
-  if(req.timedout) {
-    console.error('请求超时: url=%s, timeout=%d, 请确认方法执行耗时很长，或没有正确的 response 回调。', req.originalUrl, err.timeout);
-  }
-  res.status(statusCode);
-  // 默认不输出异常详情
-  var error = {}
-  if (app.get('env') === 'development') {
-    // 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
-    error = err;
-  }
-  res.render('error', {
-    message: err.message,
-    error: error
-  });
+app.use(function (err, req, res, next) { // jshint ignore:line
+    var statusCode = err.status || 500;
+    if (statusCode === 500) {
+        console.error(err.stack || err);
+    }
+    if (req.timedout) {
+        console.error('请求超时: url=%s, timeout=%d, 请确认方法执行耗时很长，或没有正确的 response 回调。', req.originalUrl, err.timeout);
+    }
+    res.status(statusCode);
+    // 默认不输出异常详情
+    var error = {}
+    if (app.get('env') === 'development') {
+        // 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
+        error = err;
+    }
+    res.render('error', {
+        message: err.message,
+        error: error
+    });
 });
 
 module.exports = app;
--- cloud.js
+++ cloud.js
@@ -3,8 +3,7 @@ var AV = require('leanengine');
 /**
  * 一个简单的云代码方法
  */
-AV.Cloud.define('hello', function(request, response) {
-  response.success('Hello world!');
+AV.Cloud.define('nlm.gene', function (request, response) {
 });
 
 module.exports = AV.Cloud;
